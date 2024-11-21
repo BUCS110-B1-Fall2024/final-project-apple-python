2b89adc2ea3d53490848dc3b665834099b75fd8d
AJ1025 <ajenkins1@binghamton.edu>
2024-11-20 21:17:16 -0500

    Doc Strings

--- template_final_project-master/src/buttons.py
+++ template_final_project-master/src/buttons.py
@@ -2,6 +2,16 @@ import pygame
 
 class Buttons:
     def __init__(self, x, y, width, height, color = '', display = ''):
+        """Allows for the creation of buttons
+
+        Args:
+            x (int): x-coor. for the button
+            y (int): y-coor. for the button
+            width (int): Width of the button
+            height (int): Height of the button
+            color (str, optional): The color of the button. Defaults to nothing.
+            display (str, optional): The message displayed onto the buttons surface. Defaults to nothing.
+        """
         self.x = x
         self.y = y
         self.width = width
@@ -10,7 +20,20 @@ class Buttons:
         self.display = display
 
     def create(self, screen):
+        """Creates the button on screen
+
+        Args:
+            screen (str): Name of the pyagme screen instance
+        """
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
     
     def is_clicked(self, mouse_pos):
-        return self.x <= mouse_pos[0] <= self.x + self.width and self.y <= mouse_pos[1] <= self.y + self.height
+        """Checks to see whether or not the button has been clicked
+
+        Args:
+            mouse_pos (int): postion of the mouse
+
+        Returns:
+            self.x (int): returns whether the mouse has been clicked inside of the button 
+        """
+        return self.rect.collidepoint(mouse_pos)
--- template_final_project-master/src/images.py
+++ template_final_project-master/src/images.py
@@ -1,5 +1,25 @@
+import pygame
+
 class Images:
-    def __init__(self, screen, pic):
+    def __init__(self, screen, pic_path):
+        """Allows an image from a file to be put onto the screen
+
+        Args:
+            screen (str): Where in pyagme the pic will be put
+            pic_path (str): File pathway to the picture being blit onto the screen
+        """
         self.screen = screen
-        self.pic = pic
+        self.pic_path = pic_path
+    
+    def load(self, pic_path):
+        """Using the picture pathway it loads the image
+
+        Args:
+            pic_path (str): The file pathway
+        """
+        self.pic = pygame.image.load(pic_path)
+    
+    def blit(self):
+        self.screen.blit(self.pic, (0,0))
+        pygame.display.flip()
         
