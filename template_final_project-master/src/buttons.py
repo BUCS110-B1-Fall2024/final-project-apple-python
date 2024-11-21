a5fd8120d04db927f59b17b5e9ab526c9fd630b1
KnownUnkownz <jkaufman1@binghamton.edu>
2024-11-20 21:47:53 -0500

    Update buttons.py
    no changes made, added take on button class, my button class is hash-tagged as to not interfere with previous code.

--- template_final_project-master/src/buttons.py
+++ template_final_project-master/src/buttons.py
@@ -37,3 +37,39 @@ class Buttons:
             self.x (int): returns whether the mouse has been clicked inside of the button 
         """
         return self.rect.collidepoint(mouse_pos)
+
+
+
+
+#class buttons:
+#    def __init__(self, shape, x,y ,type, color,text,font_size=12, t_color=white, mediapath=None,):
+#        self.shape = shape
+#        self.x = x
+#        self.y = y
+#        self.width = 200
+#        self.height = 60
+#        self.type = type
+#        self.color = color
+#        self.text = text
+#        self.font = pygame.font.Font(None,font_size)
+#        self.t_color = t_color
+#        self.mediapath = mediapath
+#        return self.shape, self.coords, self.type,self.color,self.text,
+#    def drawing(self,screen):'
+#        pygame.draw.rect(screen,self.color,self.rect)
+#    def clicking(self,event):
+#        if event.type == pygame.MOUSEBUTTONDOWN:
+#            if self.rect.collidepoint(event.pos):
+#                return True
+#        return False
+#    def displayvid(self):
+#        if self.mediapath:
+#            #iouno, will look further into video displayers
+#    def displayimg(self,screen):
+#        if self.mediapath:
+#         try:
+#             image = pygame.image.load(self.mediapath)
+#             
+#         except pygame.error:
+#    def displayinfo(self, screen,info):
+#        #display a text box at the top of the screen
