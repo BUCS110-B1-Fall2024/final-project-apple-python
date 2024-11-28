d17d015e8951185f7b57b923313b536bb727c0bd
KnownUnkownz <jkaufman1@binghamton.edu>
2024-11-27 21:11:54 -0500

    Update buttons.py
    made different classes for buttons that serve different purposes

--- template_final_project-master/src/buttons.py
+++ template_final_project-master/src/buttons.py
@@ -1,83 +1,73 @@
 import pygame
 
-class Buttons:
-    def __init__(self, x, y, width, height, color = '', display = ''):
-        """Allows for the creation of buttons
-
-        Args:
-            x (int): x-coor. for the button
-            y (int): y-coor. for the button
-            width (int): Width of the button
-            height (int): Height of the button
-            color (str, optional): The color of the button. Defaults to nothing.
-            display (str, optional): The message displayed onto the buttons surface. Defaults to nothing.
-        """
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.display = display
-        self.rect = pygame.Rect(x, y, width, height)
-
-    def create(self, screen):
-        """Creates the button on screen
-
-        Args:
-            screen (str): Name of the pyagme screen instance
-        """
+class buttonimages():
+    def __init__(self,x,y,w,h,color,text,font):
+         self.rect = pygame.Rect(x,y,w,h) #makes a button at these coordinates with these dimensions
+         self.color = color  #colors of button
+         self.text = text #button text
+         self.font = font #button font
+         self.text_surf=self.font.render(self.text, True, (0,0,0)) #text inside the button
+         self.text_rect=self.text_surf.get_rect(center=self.rect.center) #centers itself in the button
+    def display_button(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
-        if self.display:
-            font = pygame.font.Font(None, 15)
-            surface = font.render(self.display, True, 'black')
-            textrect = surface.get_rect(center = (self.x +self.width // 2, self.y + self.height // 2))
-            screen.blit(surface, textrect)
-        
-        
+        screen.blit(self.text_surf, self.text_rect)
+
+    def clicked(self, position):
+        return self.rect.collidepoint(position)
+    class displayimage():
+        def __init__(self,screen):
+            self.screen = screen
+        def show_image(self, image_path, x, y):
+            image = pygame.image.load(image_path).convert_alpha()
+            image = pygame.transform.scale(image, (800, 600))  # Resize if necessary
+            self.screen.blit(image, (0, 0))
     
-    def is_clicked(self, mouse_pos):
-        """Checks to see whether or not the button has been clicked
+class buttonvideos():
+    def __init__(self,x,y,w,h,color,text,font):
+         self.rect = pygame.Rect(x,y,w,h) #makes a button at these coordinates with these dimensions
+         self.color = color  #colors of button
+         self.text = text #button text
+         self.font = font #button font
+         self.text_surf=self.font.render(self.text, True, (0,0,0)) #text inside the button
+         self.text_rect=self.text_surf.get_rect(center=self.rect.center) #centers itself in the button
+    def display_button(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+        screen.blit(self.text_surf, self.text_rect)
 
-        Args:
-            mouse_pos (int): postion of the mouse
+    def clicked(self, position):
+        return self.rect.collidepoint(position)
+    class displayvideo():
+        #placeholder
+        pass
 
-        Returns:
-            self.x (int): returns whether the mouse has been clicked inside of the button 
-        """
-        return self.rect.collidepoint(mouse_pos)
+class buttontextbox():
+    def __init__(self,x,y,w,h,color,text,font):
+         self.rect = pygame.Rect(x,y,w,h) #makes a button at these coordinates with these dimensions
+         self.color = color  #colors of button
+         self.text = text #button text
+         self.font = font #button font
+         self.text_surf=self.font.render(self.text, True, (0,0,0)) #text inside the button
+         self.text_rect=self.text_surf.get_rect(center=self.rect.center) #centers itself in the button
+    def display_button(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+        screen.blit(self.text_surf, self.text_rect)
 
-
-
-
-#class buttons:
-#    def __init__(self, shape, x,y ,type, color,text,font_size=12, t_color=white, mediapath=None,):
-#        self.shape = shape
-#        self.x = x
-#        self.y = y
-#        self.width = 200
-#        self.height = 60
-#        self.type = type
-#        self.color = color
-#        self.text = text
-#        self.font = pygame.font.Font(None,font_size)
-#        self.t_color = t_color
-#        self.mediapath = mediapath
-#        return self.shape, self.coords, self.type,self.color,self.text,
-#    def drawing(self,screen):'
-#        pygame.draw.rect(screen,self.color,self.rect)
-#    def clicking(self,event):
-#        if event.type == pygame.MOUSEBUTTONDOWN:
-#            if self.rect.collidepoint(event.pos):
-#                return True
-#        return False
-#    def displayvid(self):
-#        if self.mediapath:
-#            #iouno, will look further into video displayers
-#    def displayimg(self,screen):
-#        if self.mediapath:
-#         try:
-#             image = pygame.image.load(self.mediapath)
-#             
-#         except pygame.error:
-#    def displayinfo(self, screen,info):
-#        #display a text box at the top of the screen
+    def clicked(self, position):
+        return self.rect.collidepoint(position)
+    class textboxdisplay():
+        def __init__(self, x,y,w,h,text_size,text_color,back_color=None ):
+            self.x=x
+            self.y=y #coordinates of the text
+            self.width=w 
+            self.height=h #dimensions of the text box (both coordinates and dimensions should be same to make life simpler)
+            self.font=pygame.font.Font(None,text_size) #how big the text is
+            self.text_color=text_color #color of the text (should be white or black imo)
+            self.back_color=back_color #background color of the textbox
+            self.text=""#must be initially empty
+        def text(self,text):
+            self.text=text
+            self.text_surf= self.font.render(self.text,True,self.text_color)
+        def display(self,screen):
+            if self.back_color:
+                pygame.draw.rect(screen,self.back_color,self.x,self.y,self.width,self.height)
+            screen.blit(self.text_surf,(self.x+5,self.y+5))
