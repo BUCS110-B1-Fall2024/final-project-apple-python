9551e978705d3d9f83017aedf078069e5acd6262
KnownUnkownz <jkaufman1@binghamton.edu>
2024-11-29 18:29:17 -0500

    experimental qutibeginbutton and state classes.
    need to make a back button, begin button, quit button, need to find ways to get rid of the previous set of buttons and add the new set of buttons after clicking a button, need to make a controller class.

--- template_final_project-master/src/buttons.py
+++ template_final_project-master/src/buttons.py
@@ -63,11 +63,42 @@ class buttontextbox():
             self.font=pygame.font.Font(None,text_size) #how big the text is
             self.text_color=text_color #color of the text (should be white or black imo)
             self.back_color=back_color #background color of the textbox
-            self.text=""#must be initially empty
+            self.text=""#must be initially empty(?)
         def text(self,text):
             self.text=text
             self.text_surf= self.font.render(self.text,True,self.text_color)
+
+
         def display(self,screen):
             if self.back_color:
                 pygame.draw.rect(screen,self.back_color,self.x,self.y,self.width,self.height)
             screen.blit(self.text_surf,(self.x+5,self.y+5))
+
+class quitbeginbutton(): #just a simple button, because its simple we can hardcode its trait to quit the game or go back to the start outside of the function (I believe)
+        def __init__(self,x,y,w,h,color,text,font):
+            self.rect=pygame.Rect(x,y,w,h)
+            self.color= color
+            self.text = text
+            self.font = font
+            self.text_surf=self.font.render(self.text,True,(0,0,0))
+            self.text_rect=self.text_surf.get_rect(center =self.rect.center)
+        def display(self,screen):
+            pygame.draw.rect(screen,self.color,self.rect)
+            screen.blit(self.text_surf,self.text_rect)
+        def clicked(self,pos):
+            return self.rect.collidepoint(pos)
+class State(): #???? Iouno maybe maybe 
+    def __init__(self):
+        self.current_buttons=[]
+        self.previous_buttons=[]
+    def addcurrent(self,button):
+        
+        self.current_buttons.append(button)    
+    def removecurrent(self,button): 
+        self.current_buttons.remove(button)
+    def addprevious(self,button):
+        self.previous_buttons.append(button)
+    def removeprevious(self,button):
+        self.previous_buttons.remove(button)
+#need to make a back button, need to find a way to make previous buttons disappear and new buttons appear(same goes for textboxes), need to make a controller,
+#maybe for making new buttons and old buttons appearing and disappearing make "States?", ex. I click a button the state of the program changes and loads data that was stored?
