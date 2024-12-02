48bc909c04739f0b3b903c07b52c614e711ecf5e
AJ1025 <ajenkins1@binghamton.edu>
2024-12-02 10:06:42 -0500

    Some buttons redone

--- template_final_project-master/src/buttons.py
+++ template_final_project-master/src/buttons.py
@@ -1,20 +1,47 @@
 import pygame
 
-class buttonimages():
-    def __init__(self,x,y,w,h,color,text,font):
+class Buttons():   #https://docs.python.org/3/tutorial/classes.html < how to set them up 
+    ## if you change the class name then you need to change it in the controller too
+    def __init__(self, x, y, w, h, color='', text =''):
+         """Allows for the creation of buttons
+        Args:
+            x (int): x-coor. for the button
+            y (int): y-coor. for the button
+            width (int): Width of the button
+            height (int): Height of the button
+            color (str, optional): The color of the button. Defaults to nothing.
+            display (str, optional): The message displayed onto the buttons surface. Defaults to nothing.
+        """
          self.rect = pygame.Rect(x,y,w,h) #makes a button at these coordinates with these dimensions
          self.color = color  #colors of button
          self.text = text #button text
-         self.font = font #button font
-         self.text_surf=self.font.render(self.text, True, (0,0,0)) #text inside the button
-         self.text_rect=self.text_surf.get_rect(center=self.rect.center) #centers itself in the button
-    def display_button(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
-        screen.blit(self.text_surf, self.text_rect)
 
-    def clicked(self, position):
-        return self.rect.collidepoint(position)
-    class displayimage():
+    def display_button(self, screen):
+        """Creates the button on screen
+        Args:
+            screen (str): Name of the pyagme screen instance
+        """
+        pygame.draw.rect(screen, self.color, self.rect) #Creates the button using this method
+        
+        if self.text: #if there is text for the button this is called
+            font = pygame.font.Font(None, 15) #sets the font
+            surface = font.render(self.display, True, 'black') #Sets surface and color
+            textrect = surface.get_rect(center = (self.x +self.width // 2, self.y + self.height // 2)) #Puts text in center
+            screen.blit(surface, textrect) #Puts it onto screen
+
+    def is_clicked(self, mouse_pos):
+        """Checks to see whether or not the button has been clicked
+
+        Args:
+            mouse_pos (int): position of the mouse
+
+        Returns:
+            self.rect.collidepoint (int): returns whether the mouse has been clicked inside the button
+        """
+        return self.rect.collidepoint(mouse_pos)
+    
+    
+    class displayimage(): #This can be done using the images class and just putting in the controller "if x button is clicked display x image"
         def __init__(self,screen):
             self.screen = screen
         def show_image(self, image_path, x, y):
